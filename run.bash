b763f0ff3c4963c89c11eb0946f82e0f4c25b11a
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-14 09:54:39 -0500

    pipeline snip

--- run.bash
+++ run.bash
@@ -1,7 +1,7 @@
 #cp lisp15.py temp.py
 cdir=`pwd`
-../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-constants.glue --support=${cdir}/support.js --exclusive <lisp15.scn >temp1a
-../prep/prep '.' '$' snip.ohm snip.glue --stop=1 --support=${cdir}/support.js --exclusive <temp1a >temp1.py
+../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-constants.glue --support=${cdir}/support.js --exclusive <lisp15.scn \
+	     | ../prep/prep '.' '$' snip.ohm snip.glue --stop=1 --support=${cdir}/support.js --exclusive <temp1a >temp1.py
 # ../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-initializers.glue --support=${cdir}/support.js --exclusive <lisp15.scn >temp2.py
 # ../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-rm.glue --support=${cdir}/support.js --exclusive <lisp15.scn >temp3.py
 # cat temp1.py temp2.py temp3.py >temp.py
