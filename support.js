b8bbcf0bbeea3e5ad73c2569498fa69ab2fc75a7
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-14 12:38:42 -0500

    temp.py contains 0's

--- support.js
+++ support.js
@@ -4,10 +4,18 @@ exports.getAtomIndex = function () {
     return `${atomIndex}`;
 }
 
-exports.pokeNameIntoAtomArray = function (s) {
+exports.pokeNameIntoAtomArray = function (untrimmeds) {
+    let s = untrimmeds.trim ()
     result = ""
+    let final = (2 * s.length) + atomIndex - 2;
+    console.error (final)
     for (c of s) {
-	result += `'${c}', ${atomIndex+1},\n`;
+	if (atomIndex < final) {
+	    n = atomIndex + 2;
+	} else {
+	    n = 0;
+	}	
+	result += `'${c}', ${n},\n`;
 	atomIndex += 2;
     }
     return result;
