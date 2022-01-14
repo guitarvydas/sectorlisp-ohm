7d2b3a38b49bd425573b4a5648615b6d17e3965e
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-14 09:57:59 -0500

    initializers

--- run.bash
+++ run.bash
@@ -1,8 +1,9 @@
 #cp lisp15.py temp.py
 cdir=`pwd`
 ../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-constants.glue --support=${cdir}/support.js --exclusive <lisp15.scn \
-	     | ../prep/prep '.' '$' snip.ohm snip.glue --stop=1 --support=${cdir}/support.js --exclusive <temp1a >temp1.py
-# ../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-initializers.glue --support=${cdir}/support.js --exclusive <lisp15.scn >temp2.py
+	     | ../prep/prep '.' '$' snip.ohm snip.glue --stop=1 --support=${cdir}/support.js --exclusive >temp1.py
+../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-initializers.glue --support=${cdir}/support.js --exclusive <lisp15.scn \
+	     | ../prep/prep '.' '$' snip.ohm snip.glue --stop=1 --support=${cdir}/support.js --exclusive >temp2.py
 # ../prep/prep '#+ atoms' '#+ ' atoms.ohm atoms-rm.glue --support=${cdir}/support.js --exclusive <lisp15.scn >temp3.py
 # cat temp1.py temp2.py temp3.py >temp.py
 #python temp.py
